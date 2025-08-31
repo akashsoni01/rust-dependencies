ffde467f97202b6f89efd4da6c3059dec8a61412
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-08-31 11:27:26 +0530

    mock added

--- examples/reqwest.rs
+++ examples/reqwest.rs
@@ -12,7 +12,7 @@ struct HttpBin {
 
 struct Environment {
     date: Box<dyn Fn() -> DateTime<Local>>,
-    http_bin: Box<dyn Fn() -> Pin<Box<dyn Future<Output = Result<HttpBin, reqwest::Error>>>>>,
+    http_bin: Box<dyn Fn(String) -> Pin<Box<dyn Future<Output = Result<HttpBin, reqwest::Error>>>>>,
 }
 
 impl Environment {
@@ -21,53 +21,77 @@ impl Environment {
 
         Self {
             date: Box::new(|| Local::now()),
-            http_bin: Box::new(move || {
+            http_bin: Box::new(move |url_string: String| {
                 let client = client.clone();
                 Box::pin(async move {
                     let response = client
-                        .get("https://httpbin.org/get")
+                        .get(url_string.as_str())
                         .send()
                         .await?
                         .json::<serde_json::Value>()
                         .await?;
-
-                    Ok(HttpBin {
+                    let bin = HttpBin {
                         origin: response["origin"]
                             .as_str()
                             .unwrap_or("unknown")
                             .to_string(),
-                    })
+                    };
+                    println!("prod = {:?}", bin);
+                    Ok(bin)
                 })
             }),
         }
     }
 
-    // async fn get_http_bin_data(&self) -> Result<HttpBin, reqwest::Error> {
-    //     let client = Client::new();
-    //     let response = client
-    //         .get("https://httpbin.org/get")
-    //         .send()
-    //         .await?
-    //         .json::<serde_json::Value>()
-    //         .await?;
-    // 
-    //     Ok(HttpBin {
-    //         origin: response["origin"]
-    //             .as_str()
-    //             .unwrap_or("unknown")
-    //             .to_string(),
-    //     })
-    // }
+    fn mock() -> Self {
+        let mock_date = DateTime::<Local>::from(std::time::UNIX_EPOCH + std::time::Duration::from_secs(557152051));
+        
+        Self {
+            date: Box::new(move || mock_date),
+            http_bin: Box::new(|url_string: String| {
+                let mock_bin = HttpBin {
+                    origin: "mock".to_string(),
+                };
 
+                Box::pin(async move {
+                    println!("mock = {:?}", mock_bin);
+                    Ok(mock_bin)
+                })
+            }),
+        }
+    }
 }
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+async fn main()  {
+    let env = Environment::new();
+    (env.http_bin)("https://httpbin.org/get".to_string()).await.unwrap();
+    println!(
+        "Hello, world! at {}",
+        (env.date)().format("%Y-%m-%d %H:%M:%S")
+    );
+    
+    let mock = Environment::mock();
+    (mock.http_bin)("https://httpbin.org/get".to_string()).await.unwrap();
+    println!(
+        "Hello, world! at {}",
+        (mock.date)().format("%Y-%m-%d %H:%M:%S")
+    );
 }
+
+/*
+Output call 1 
+prod = HttpBin { origin: "103.48.69.244" }
+Hello, world! at 2025-08-31 11:26:47
+mock = HttpBin { origin: "mock" }
+Hello, world! at 1987-08-28 17:57:31
+
+
+Output call 2
+prod = HttpBin { origin: "103.48.69.244" }
+Hello, world! at 2025-08-31 11:26:58
+mock = HttpBin { origin: "mock" }
+Hello, world! at 1987-08-28 17:57:31
+
+*/
